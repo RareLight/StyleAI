57bd17ae333e592663e97e09ef705bc15f9e8cce
Bastian Machek <16717398+bmachek@users.noreply.github.com>
2026-01-18 16:45:46 +0100

    Refactor UpdateCheck.checkForNewVersionInBackground to call checkForNewVersion directly

--- LrGeniusAI.lrdevplugin/UpdateCheck.lua
+++ LrGeniusAI.lrdevplugin/UpdateCheck.lua
@@ -32,26 +32,5 @@ function UpdateCheck.checkForNewVersion()
 end
 
 function UpdateCheck.checkForNewVersionInBackground()
-    local response, headers = LrHttp.get(UpdateCheck.updateCheckUrl)
-
-    if headers.status == 200 then
-        if response ~= nil then
-            local decoded = JSON:decode(response)
-            if decoded ~= nil then
-                if decoded.tag_name ~= UpdateCheck.releaseTagName then
-                    LrHttp.openUrlInBrowser(UpdateCheck.latestReleaseUrl)
-                else
-                    LrDialogs.message("You're on the latest plugin version: " .. UpdateCheck.releaseTagName)
-                end
-            end
-        else
-            log:error('Could not run update check. Empty response')
-        end
-    else
-        log:error('Update check failed. ' .. UpdateCheck.updateCheckUrl)
-        log:error(Util.dumpTable(headers))
-        log:error(response)
-        return nil
-    end
-    return nil
+    return UpdateCheck.checkForNewVersion()
 end
