a95178aa906351215ca6a17f4bdfe98665d96dbf
Bastian Machek <16717398+bmachek@users.noreply.github.com>
2026-04-11 12:24:02 +0200

    refactor: update shutdown logic to unload resources from both local and remote backends

--- plugin/LrGeniusAI.lrdevplugin/ShutdownApp.lua
+++ plugin/LrGeniusAI.lrdevplugin/ShutdownApp.lua
@@ -1,16 +1,12 @@
 local function shutdownApp(doneFunc, progressFunc)
-    -- Instead of shutting down the backend, we now only unload models and collections from memory
-    -- to free up resources while keeping the service running.
-    if SearchIndexAPI.isBackendOnLocalhost() then
-        LrTasks.startAsyncTask(function()
-            LrTasks.pcall(function()
-                SearchIndexAPI.unloadResources()
-            end)
-            doneFunc()
+    -- Instead of shutting down the backend, we now request it to unload models and collections from memory
+    -- to free up resources. This is sent to both local and remote backends to ensure efficiency.
+    LrTasks.startAsyncTask(function()
+        LrTasks.pcall(function()
+            SearchIndexAPI.unloadResources()
         end)
-    else
         doneFunc()
-    end
+    end)
 end
 
 return {
